396298b043941f3c03682ab4d7d83908e0f6baf1
MC-Chung <hermanchungmc@gmail.com>
2018-09-09 11:50:15 +0800

    changed  rb

--- lib/jones_tool.rb
+++ lib/jones_tool.rb
@@ -1,11 +1,12 @@
 require "jones_tool/version"
 
 module JonesTool
-  class Copyier
+	class Copyier
 		def self.copy name, msg
 
 			"&copy; #{Time.now.year} | #{name} #{msg} ".html_safe
 
 		end
 	end
+  
 end
